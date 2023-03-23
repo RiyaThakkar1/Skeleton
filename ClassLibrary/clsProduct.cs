341264359c1fa48db7aac694d33729de60cf4169
Pragnesh Jaendra <P2664813@my365.dmu.ac.uk>
2023-03-23 17:51:41 +0000

    All the test pased

--- ClassLibrary/clsProduct.cs
+++ ClassLibrary/clsProduct.cs
@@ -4,12 +4,12 @@ namespace ClassLibrary
 {
     public class clsProduct
     {
-        public bool Active { get; set; }
         public DateTime DateAdded { get; set; }
         public int Product_ID { get; set; }
         public string Product_Name { get; set; }
         public string Product_Description { get; set; }
         public int Producct_Price { get; set; }
         public DateTime Launch_Data { get; set; }
+        public bool Product_Availability { get; set; }
     }
 }
--- Testing1/tstProduct.cs
+++ Testing1/tstProduct.cs
@@ -16,16 +16,16 @@ namespace Testing1
             Assert.IsNotNull(Anproduct);
         }
         [TestMethod]
-        public void ActivePropertyOK()
+        public void Product_AvailabilityePropertyOK()
         {
             //create an instance of the class we want to create
             clsProduct AnProduct = new clsProduct();
             //create some test data to assign to the property
             Boolean TestData = true;
             //assign the data to the property
-            AnProduct.Active = TestData;
+            AnProduct.Product_Availability = TestData;
             //test to see that the two values are the same
-            Assert.AreEqual(AnProduct.Active, TestData);
+            Assert.AreEqual(AnProduct.Product_Availability, TestData);
         }
         [TestMethod]
         public void DateAddedPropertyOK()
