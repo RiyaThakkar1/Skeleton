263b04bab0c87e4df478a615c1a1f7725ce93860
Pragnesh Jaendra <P2664813@my365.dmu.ac.uk>
2023-03-23 16:32:45 +0000

    All the test done

--- ClassLibrary/clsProduct.cs
+++ ClassLibrary/clsProduct.cs
@@ -1,6 +1,15 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsProduct
     {
+        public bool Active { get; set; }
+        public DateTime DateAdded { get; set; }
+        public int Product_ID { get; set; }
+        public string Product_Name { get; set; }
+        public string Product_Description { get; set; }
+        public int Producct_Price { get; set; }
+        public DateTime Launch_Data { get; set; }
     }
 }
--- Testing1/tstProduct.cs
+++ Testing1/tstProduct.cs
@@ -15,7 +15,91 @@ namespace Testing1
             //test to see that it exits
             Assert.IsNotNull(Anproduct);
         }
+        [TestMethod]
+        public void ActivePropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsProduct AnProduct = new clsProduct();
+            //create some test data to assign to the property
+            Boolean TestData = true;
+            //assign the data to the property
+            AnProduct.Active = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnProduct.Active, TestData);
+        }
+        [TestMethod]
+        public void DateAddedPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsProduct AnProduct = new clsProduct();
+            //create some test data to assign to the property
+            DateTime TestData = DateTime.Now.Date;
+            //assign the data to the property
+            AnProduct.DateAdded = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnProduct.DateAdded, TestData);
+        }
+        [TestMethod]
+        public void Product_IDPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsProduct AnProduct = new clsProduct();
+            //create some test data to assign to the property
+            Int32 TestData = 1;
+            //assign the data to the property
+            AnProduct.Product_ID = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnProduct.Product_ID, TestData);
+        }
+        [TestMethod]
+        public void Product_NamePropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsProduct AnProduct = new clsProduct();
+            //create some test data to assign to the property
+            string TestData = "";
+            //assign the data to the property
+            AnProduct.Product_Name = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnProduct.Product_Name, TestData);
+        }
+        [TestMethod]
+        public void Product_DescriptionPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsProduct AnProduct = new clsProduct();
+            //create some test data to assign to the property
+            string TestData = "Some Street";
+            //assign the data to the property
+            AnProduct.Product_Description = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnProduct.Product_Description, TestData);
+        }
+        [TestMethod]
+        public void Producct_PricePropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsProduct AnProduct = new clsProduct();
+            //create some test data to assign to the property
+            Int32 TestData = 1;
+            //assign the data to the property
+            AnProduct.Producct_Price = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnProduct.Producct_Price, TestData);
+        }
+        [TestMethod]
+        public void Launch_DataPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsProduct AnProduct = new clsProduct();
+            //create some test data to assign to the property
+            DateTime TestData = DateTime.Now.Date;
+            //assign the data to the property
+            AnProduct.Launch_Data = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(AnProduct.Launch_Data, TestData);
+        }
 
-        
     }
+
 }
