ab23fb91ca821beaba546a16e35968296e6273d0
P2666368 <P2666368@GWHXX284WIN017W>
2023-03-23 15:49:18 +0000

    Added test for Orders class

---
+++ Testing5/tstOrders.cs
@@ -0,0 +1,20 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing5
+{
+    [TestClass]
+    public class tstOrders
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance of the class we want to create
+            clsOrders AnOrder = new clsOrders();
+            //test to see if it exists
+            Assert.IsNotNull(AnOrder);
+        }
+    }
+}
+
