6a3eade1a74575e83e5e20ecc5c4f8a8946ab62f
Pragnesh Jaendra <P2664813@my365.dmu.ac.uk>
2023-03-23 15:52:17 +0000

    Tested my product class

---
+++ Testing1/tstProduct.cs
@@ -0,0 +1,19 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing1
+{
+    [TestClass]
+    public class tstProduct
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance of the class we want to create
+            clsproduct Anproduct = new clsproduct();
+            //test to see that it exits
+            Assert.IsNotNull(Anproduct);
+        }
+    }
+}
