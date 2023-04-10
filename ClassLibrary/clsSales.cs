e5d50808130beb77f47392d05c2c7ed3ba5aca52
Kavipiriya Thamilmaran <P2662650@my365.dmu.ac.uk>
2023-04-10 15:17:41 +0100

    Created more tests for Sales class

--- ClassLibrary/clsSales.cs
+++ ClassLibrary/clsSales.cs
@@ -1,6 +1,17 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsSales
     {
+        public bool Active { get; set; }
+        public string CustomerFirstName { get; set; }
+        public DateTime CustomerDOB { get; set; }
+        public string CustomerLastName { get; set; }
+        public string CustomerEmailID { get; set; }
+        public string CustomerContactNumber { get; set; }
+        public string OrderID { get; set; }
+        public string OrderQuantity { get; set; }
+        public string OrderDescription { get; set; }
     }
 }
