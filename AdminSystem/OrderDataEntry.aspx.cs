a6020ca6c5b83166e9d516c6ffc4246601da14d2
P2666368 <P2666368@GWHXX284WIN017W>
2023-04-17 20:41:19 +0100

    Find Method completed

--- AdminSystem/OrderDataEntry.aspx.cs
+++ AdminSystem/OrderDataEntry.aspx.cs
@@ -15,13 +15,38 @@ public partial class _1_DataEntry : System.Web.UI.Page
 
     protected void btnOK_Click(object sender, EventArgs e)
     {
-       //create a new instance of clsOrders
-       clsOrders AnOrder = new clsOrders();
-       //capture the OrderName
-       AnOrder.OrderName = txtOrderName.Text;
-       //store the order in the session object
-       Session["AnOrder"] = AnOrder;
-       //navigate to the viewer panner
-       Response.Redirect("OrderViewer.aspx");
+        //create a new instance of clsOrders
+        clsOrders AnOrder = new clsOrders();
+        //capture the OrderName
+        AnOrder.OrderName = txtOrderName.Text;
+        //store the order in the session object
+        Session["AnOrder"] = AnOrder;
+        //navigate to the viewer panner
+        Response.Redirect("OrderViewer.aspx");
+    }
+
+    protected void btnFind_Click(object sender, EventArgs e)
+    {
+        {
+            //create an instance of the class we want to create
+            clsOrders AnOrder = new clsOrders();
+            //variable to store the primary key
+            Int32 OrderID;
+            //variable to store the result of the find operation
+            Boolean Found = false;
+            //get the primary key entered by the user
+            OrderID = Convert.ToInt32(txtOrderID.Text);
+            //find the record
+            Found = AnOrder.Find(OrderID);
+            //if found
+            if (Found == true)
+            {
+                //display the value of the properties in the form
+                txtOrderName.Text = AnOrder.OrderName;
+                txtOrderPrice.Text = AnOrder.OrderPrice.ToString();
+                txtOrderDate.Text = AnOrder.OrderDate.ToString();
+                txtCustomerID.Text = AnOrder.CustomerID.ToString();
+            }
+        }
     }
 }
--- ClassLibrary/clsOrders.cs
+++ ClassLibrary/clsOrders.cs
@@ -32,7 +32,8 @@ namespace ClassLibrary
             }
         }
         public decimal mOrderPrice;
-        public decimal OrderPrice { 
+        public decimal OrderPrice
+        {
             get
             {
                 return mOrderPrice;
@@ -43,7 +44,8 @@ namespace ClassLibrary
             }
         }
         public DateTime mOrderDate;
-        public DateTime OrderDate {
+        public DateTime OrderDate
+        {
             get
             {
                 return mOrderDate;
@@ -53,9 +55,10 @@ namespace ClassLibrary
             {
                 mOrderDate = value;
             }
-         }
+        }
         public int mCustomerID;
-        public int CustomerID {
+        public int CustomerID
+        {
             get
             {
                 return mCustomerID;
@@ -64,18 +67,35 @@ namespace ClassLibrary
             {
                 mCustomerID = value;
             }
-                }
+        }
 
         public bool Find(int orderID)
         {
-            //set the private data members to the test data value
-            mOrderID = 21;
-            mOrderName = "First Order";
-            mOrderPrice = 30;
-            mOrderDate = DateTime.Now;
-            mCustomerID = 1;
-            //Always return true
-            return true;
+
+            //create an instance of the data connection
+            clsDataConnection DB = new clsDataConnection();
+            //add the parameter for the OrderID to search for
+            DB.AddParameter("@OrderID", OrderID);
+            //execute the stored procedure
+            DB.Execute("sproc_tblOrder_FilterByOrderID");
+            //if one record is found (there sould be either one or zero!)
+            if (DB.Count == 1)
+            {
+                //copy the data from the database to the private data members
+                mOrderID = Convert.ToInt32(DB.DataTable.Rows[0]["OrderID"]);
+                mOrderName = Convert.ToString(DB.DataTable.Rows[0]["OrderName"]);
+                mOrderPrice = Convert.ToDecimal(DB.DataTable.Rows[0]["OrderPrice"]);
+                mOrderDate = Convert.ToDateTime(DB.DataTable.Rows[0]["OrderDate"]);
+                mCustomerID = Convert.ToInt32(DB.DataTable.Rows[0]["CustomerID"]);
+                //return that everything worked ok
+                return true;
+            }
+            //if no record was found
+            else
+            {
+                //return false indicating a problem
+                return false;
+            }
         }
     }
 }
