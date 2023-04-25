fcdd35506a41adba59dcd4befac3301f18dc17e8
Najeeb Khan <P2662092@my365.dmu.ac.uk>
2023-04-25 13:40:55 +0100

    Added testing for delivery class

---
+++ Testing6/tblDelivery.cs
@@ -0,0 +1,21 @@
+﻿using ClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testing6
+{
+    class tblDelivery
+    {
+        [TestMethod]
+        public void instanceOK()
+        {//create an instance of the class we want to create
+            clsDelivery AnDelivery = new clsDelivery();
+            //test to see that it exists
+            Assert.IsNotNull(AnDelivery);
+
+        }
+    }
+}
